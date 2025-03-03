b002c65e4570bdef3fd682ce574352913048cf2e
masterravi01 <raviparmar2288@gmail.com>
2025-03-03 11:31:27 +0000

    business master page added

--- src/shared/components/business-master-modal/business-master-modal.component.ts
+++ src/shared/components/business-master-modal/business-master-modal.component.ts
@@ -47,7 +47,85 @@ export class BusinessMasterModalComponent {
     { name: 'Jane Smith', email: 'jane@example.com', phone: '987-654-3210' },
     { name: 'John Doe', email: 'john@example.com', phone: '123-456-7890' },
   ];
+  instructions = [
+    { id: '1', value: 'this is base instructions' },
+    { id: '2', value: 'this is base instructions' },
+    { id: '4', value: 'this is base instructions' },
+    { id: '5', value: 'this is base instructions' },
+    { id: '9', value: 'this is base instructions' },
+  ];
+  containers = [
+    {
+      id: '4',
+      name: 'Box 201',
+      type: 'Full Size',
+      width: '124',
+      height: '200',
+      weight: '300',
+      length: '100',
+    },
+    {
+      id: '4',
+      name: 'Box 201',
+      type: 'Full Size',
+      width: '124',
+      height: '200',
+      weight: '300',
+      length: '100',
+    },
+    {
+      id: '4',
+      name: 'Box 201',
+      type: 'Full Size',
+      width: '124',
+      height: '200',
+      weight: '300',
+      length: '100',
+    },
+    {
+      id: '4',
+      name: 'Box 201',
+      type: 'Full Size',
+      width: '124',
+      height: '200',
+      weight: '300',
+      length: '100',
+    },
+  ];
 
+  currency = [
+    {
+      id: '4',
+      name: 'US Dollar',
+      char: '$',
+      country: 'USA',
+    },
+    {
+      id: '4',
+      name: 'US Dollar',
+      char: '$',
+      country: 'USA',
+    },
+    {
+      id: '4',
+      name: 'US Dollar',
+      char: '$',
+      country: 'USA',
+    },
+    {
+      id: '4',
+      name: 'US Dollar',
+      char: '$',
+      country: 'USA',
+    },
+  ];
+  bottomNote = [
+    { id: '1', value: 'this is base bottom note !' },
+    { id: '2', value: 'this is base bottom note !' },
+    { id: '4', value: 'this is base bottom note !' },
+    { id: '5', value: 'this is base bottom note !' },
+    { id: '9', value: 'this is base bottom note !' },
+  ];
   constructor(private modalService: ModalService) {}
   onCancel(): void {
     this.dialogRef.close(false); // Return false on cancel
