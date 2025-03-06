e2916cc0094774548e9024a7ea9ff1e259760c27
masterravi01 <raviparmar2288@gmail.com>
2025-03-06 16:12:53 +0530

    system parameter component ui updated & unsubscribe flow added

--- src/shared/components/new-currency-modal/new-currency-modal.component.ts
+++ src/shared/components/new-currency-modal/new-currency-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Inject,
+  Input,
+  Output,
+} from '@angular/core';
 import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import {
   MAT_DIALOG_DATA,
@@ -15,7 +22,9 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MasterService } from '../../../core/services/master.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy(this)
 @Component({
   selector: 'app-new-currency-modal',
   standalone: true,
@@ -36,16 +45,22 @@ import { MasterService } from '../../../core/services/master.service';
 export class NewCurrencyModalComponent {
   currencyForm: FormGroup;
 
+  readonly data = inject<any>(MAT_DIALOG_DATA);
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<NewCurrencyModalComponent>,
     private masterService: MasterService
   ) {
     this.currencyForm = this.fb.group({
+      id: [0],
       currencyName: [''],
       currencyChar: [''],
       currencyCountry: [''],
     });
+    console.log(this.data);
+    if (this.data?.id) {
+      this.currencyForm.patchValue(this.data);
+    }
   }
 
   onCancel() {
@@ -53,9 +68,12 @@ export class NewCurrencyModalComponent {
   }
 
   onSave() {
-    console.log(this.currencyForm.value);
-    const sub = this.masterService
-      .invoke('addCurrency', this.currencyForm.value)
+    const callUrl = this.currencyForm.get('id')?.value
+      ? 'updateCurrency'
+      : 'addCurrency';
+    this.masterService
+      .invoke(callUrl, this.currencyForm.value)
+      .pipe(untilDestroyed(this))
       .subscribe((data) => {
         console.log(data);
       });
--- src/shared/components/system-parameter-modal/system-parameter-modal.component.ts
+++ src/shared/components/system-parameter-modal/system-parameter-modal.component.ts
@@ -1,35 +1,34 @@
-import { Component, inject, OnDestroy } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
-  MatDialog,
+  MatDialogRef,
+  MatDialogModule,
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
-  MatDialogModule,
-  MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material/table';
-import { NewCustomerComponent } from '../new-customer/new-customer.component';
-import { ModalService } from '../../../core/services/modal.service';
-import { ContainerModalComponent } from '../container-modal/container-modal.component';
-import { NewCurrencyModalComponent } from '../new-currency-modal/new-currency-modal.component';
-import { SingleParamenterComponent } from '../single-paramenter/single-paramenter.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDividerModule } from '@angular/material/divider';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { MasterService } from '../../../core/services/master.service';
-import { Subscription } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+import { NewCustomerComponent } from '../new-customer/new-customer.component';
+import { ModalService } from '../../../core/services/modal.service';
+import { NewCurrencyModalComponent } from '../new-currency-modal/new-currency-modal.component';
+import { MasterService } from '../../../core/services/master.service';
+
+@UntilDestroy()
 @Component({
   selector: 'app-system-parameter-modal',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatButtonModule,
+    MatDialogModule,
     MatDialogActions,
     MatDialogClose,
     MatDialogTitle,
@@ -44,72 +43,87 @@ import { Subscription } from 'rxjs';
   templateUrl: './system-parameter-modal.component.html',
   styleUrl: './system-parameter-modal.component.css',
 })
-export class SystemParameterModalComponent implements OnDestroy {
+export class SystemParameterModalComponent implements OnInit {
   readonly dialogRef = inject(MatDialogRef<SystemParameterModalComponent>);
   readonly data = inject<any>(MAT_DIALOG_DATA);
+
   displayedColumns: string[] = ['name', 'email', 'phone'];
   companyForm!: FormGroup;
-  currency = [];
-  private subscriptions: Subscription = new Subscription();
+  currency: any[] = [];
 
   constructor(
     private modalService: ModalService,
     private fb: FormBuilder,
     private masterService: MasterService
-  ) {
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    this.loadCompanyData();
+    this.loadCurrencies();
+  }
+  private initForm() {
     this.companyForm = this.fb.group({
       id: [1],
-      companyCode: [''], // Matches companyCode
-      companyName: [''], // Matches companyName
-      entryDate: [''], // Matches entryDate
-      currencyCode: [''], // Matches currencyCode
-      createdBy: [''], // Matches createdBy
-      remarks: [''], // Matches remarks
-      isCurrentCompany: [0], // Matches isCurrentCompany
-
-      // Company Address
-      companyAddressLine1: [''], // Matches companyAddressLine1
-      companyAddressLine2: [''], // Matches companyAddressLine2
-      companyCity: [''], // Matches companyCity
-      companyPostalCode: [''], // Matches companyPostalCode
-      companyCountry: [''], // Matches companyCountry
-      companyState: [''], // Matches companyState
-
-      // Bank Details
-      bankName: [''], // Matches bankName
-      bankAddressLine1: [''], // Matches bankAddressLine1
-      bankAddressLine2: [''], // Matches bankAddressLine2
-      bankCity: [''], // Matches bankCity
-      bankPostalCode: [''], // Matches bankPostalCode
-      bankCountry: [''], // Matches bankCountry
-      bankState: [''], // Matches bankState
-      swiftCode: [''], // Matches swiftCode
-      accountNumber: [''], // Matches accountNumber
-      additionalNumber: [''], // Matches additionalNumber
-      importExportCode: [''], // Matches importExportCode
-      taxIdentificationNumber: [''], // Matches taxIdentificationNumber
+      companyCode: [''],
+      companyName: [''],
+      entryDate: [''],
+      currencyCode: [''],
+      createdBy: [''],
+      remarks: [''],
+      isCurrentCompany: [0],
+      companyAddressLine1: [''],
+      companyAddressLine2: [''],
+      companyCity: [''],
+      companyPostalCode: [''],
+      companyCountry: [''],
+      companyState: [''],
+      bankName: [''],
+      bankAddressLine1: [''],
+      bankAddressLine2: [''],
+      bankCity: [''],
+      bankPostalCode: [''],
+      bankCountry: [''],
+      bankState: [''],
+      swiftCode: [''],
+      accountNumber: [''],
+      additionalNumber: [''],
+      importExportCode: [''],
+      taxIdentificationNumber: [''],
     });
-    const sub = this.masterService
+  }
+  private loadCompanyData() {
+    this.masterService
       .invoke('getCompany', 1)
+      .pipe(untilDestroyed(this))
       .subscribe((data: any) => {
         console.log(data);
-        this.companyForm.patchValue(data);
+        if (data) {
+          this.companyForm.patchValue(data);
+          this.companyForm.disable();
+        }
       });
-    const sub1 = this.masterService
+  }
+
+  private loadCurrencies() {
+    this.masterService
       .invoke('getAllCurrencies')
+      .pipe(untilDestroyed(this))
       .subscribe((data: any) => {
         console.log(data);
         this.currency = data;
       });
-    this.subscriptions.add(sub1);
-    this.subscriptions.add(sub);
   }
+
   onCancel(): void {
-    this.dialogRef.close(false); // Return false on cancel
+    this.dialogRef.close(false);
   }
 
   onConfirm(): void {
-    this.dialogRef.close(true); // Return true on confirm
+    this.dialogRef.close(true);
+  }
+  enableEdit() {
+    this.companyForm.enable();
   }
   openCustomerModal() {
     this.modalService.openModal(NewCustomerComponent, {
@@ -118,27 +132,34 @@ export class SystemParameterModalComponent implements OnDestroy {
     });
   }
 
-  openCurrencyModal() {
-    this.modalService.openModal(NewCurrencyModalComponent, {
-      width: '50%',
-      height: '90%',
-      position: {
-        top: '40px',
-      },
-    });
+  openCurrencyModal(data?: any) {
+    this.modalService
+      .openModal(NewCurrencyModalComponent, {
+        width: '50%',
+        minHeight: '300px',
+        position: { top: '40px' },
+        data,
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) this.loadCurrencies();
+      });
   }
+
   onSave() {
-    console.log(this.companyForm.value);
-    const sub = this.masterService
-      .invoke('updateCompany', this.companyForm.value)
+    if (this.companyForm.disabled) {
+      this.companyForm.enable();
+    }
+    const callUrl = this.companyForm.get('id')?.value
+      ? 'updateCompany'
+      : 'addCompany';
+    this.masterService
+      .invoke(callUrl, this.companyForm.value)
+      .pipe(untilDestroyed(this))
       .subscribe((data) => {
         console.log(data);
+        this.initForm();
+        this.loadCompanyData();
       });
-    this.subscriptions.add(sub);
-
-    this.dialogRef.close(this.companyForm.value);
-  }
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe(); // Prevent memory leaks
   }
 }
